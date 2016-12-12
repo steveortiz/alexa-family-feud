48f194b4a1bf688a4f27960817ed56bcb8b4763e
krismndz <krismndz7@gmail.com>
2016-12-11 21:05:24 -0600

    Removed .DS_Store
    Added Console logs to api-client.js

--- src/api-client.js
+++ src/api-client.js
@@ -1,3 +1,5 @@
 export function get(answer, callback) {
   //Does nothing right now
+  console.log("Callback in api-client: " + JSON.stringify(callback));
+  console.log("Answer in api-client: " + JSON.stringify(answer));
 }
