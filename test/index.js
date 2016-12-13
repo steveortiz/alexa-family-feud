08b167e7d4279e38c4c2f735ecbcf663a884bc89
krismndz <krismndz7@gmail.com>
2016-12-13 16:56:13 -0600

    Created a generic intent

--- test/index.js
+++ test/index.js
@@ -19,9 +19,15 @@ describe('hello world', () => {
       result.response.outputSpeech.text.should.equal('Welcome to the Alexa Skills Kit, you can say hello');
     });
   });
-  it('should reply to IntentRequest - HelloWorldIntent', (done) => {
-    testRequest(newIntentRequest('HelloWorldIntent'), done, (result) => {
-      result.response.outputSpeech.text.should.equal('Hello World!');
+  it('should reply to IntentRequest - GenericIntent', (done) => {
+    const slots = {
+      spokenText: {
+        name: 'spokenText',
+        value: 'hi',
+      },
+    };
+    testRequest(newIntentRequest('GenericIntent', slots), done, (result) => {
+      result.response.outputSpeech.text.should.equal('You said hi');
     });
   });
   it('should reply to SessionEndRequest', (done) => {
